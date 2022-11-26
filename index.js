94dbbf176dfdb145c7869d62963fcc97910bd743
i-muiri <ianmuiri99@gmail.com>
2022-11-26 17:49:25 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return(`This Saturday, I want to ${activity}!`);
+      }
+    saturdayFun();
+    saturdayFun('roller-skate');
+
+function mondayWork(activity = 'go to the office'){
+    return(`This Monday, I will ${activity}.`);
+        }
+    mondayWork('work from home.');
+
+
+    function wrapAdjective(wrapper = "*") {
+        function emphatic(string = "a hard worker") {
+        return `You are ${wrapper}${string}${wrapper}!`;
+            };
+        return emphatic;
+          };
+        let result = wrapAdjective("||");
+        result("a dedicated programmer");
